cd1769caefbf530320f60a549d9751b3007e3990
snsaleh1 <snsaleh11@gmail.com>
2019-11-07 14:27:10 -0600

    pylint error research

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
+from .models import Posts
 
-    return render(request, 'posts/index.html', {
-        'title': 'Latest Posts'
-    })
+# Create your views here.
+
+
+def index(request):
+    # return HttpResponse('Hello from posts')
+
+    posts = Posts.objects.all()[:10]
+
+    context = {
+        'title': 'Latest Posts',
+        'posts': posts
+    }
+
+    return render(request, 'posts/index.html', context)
+
+def details(request, id):
+    post = Posts.objects.get(id=id)
+
+    context = {
+            'post': post
+    }
+    return render(request, 'posts/details.html')
